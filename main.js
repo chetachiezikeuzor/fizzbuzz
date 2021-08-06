ebcf3230db9d8b0206803da8f049fd82a1d18577
Chetachi Ezikeuzor <79069364+chetachiezikeuzor@users.noreply.github.com>
2021-08-06 08:09:12 -0500

    🛠 Configured styling

--- main.js
+++ main.js
@@ -2,15 +2,15 @@ document.getElementById("inputButton").addEventListener("click", function () {
   var x = document.getElementById("textInput").value;
   var results = document.getElementById("checker");
   if (x % 5 === 0 && x % 3 === 0) {
-    results.innerHTML = `<text><em style="color: #ff85ff">"${x}"</em> FIZZBUZZ!</text>`;
+    results.innerHTML = `<text><em style="color: #0005ff">"${x}"</em> FIZZBUZZ!</text>`;
     return "fizzbuzz";
   }
   if (x % 3 === 0 && x % 5 !== 0) {
-    results.innerHTML = `<text><em style="color: #ff85ff">"${x}"</em> FIZZ</text>`;
+    results.innerHTML = `<text><em style="color: #0005ff">"${x}"</em> FIZZ</text>`;
     return "fizz";
   } else if (x % 5 === 0 && x % 3 != 0) {
-    results.innerHTML = `<text><em style="color: #ff85ff">"${x}"</em> BUZZ</text>`;
+    results.innerHTML = `<text><em style="color: #0005ff">"${x}"</em> BUZZ</text>`;
     return "buzz";
   }
-  results.innerHTML = `<text><em style="color: #ff85ff">"${x}"</em> </text>`;
+  results.innerHTML = `<text><em style="color: #0005ff">"${x}"</em> </text>`;
 });
